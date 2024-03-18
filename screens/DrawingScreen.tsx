d2c05cee3b5f272abe13dc4d6e34d68633bedc7a
Subarna Raj Upreti <usubarna21@tbc.edu.np>
2024-03-18 09:13:24 +0545

    feat:some changes

--- screens/DrawingScreen.tsx
+++ screens/DrawingScreen.tsx
@@ -6,7 +6,18 @@ import {
   GestureDetector,
   GestureHandlerRootView,
 } from "react-native-gesture-handler";
-import { Canvas, Path, useCanvasRef } from "@shopify/react-native-skia";
+import {
+  Canvas,
+  ImageFormat,
+  Path,
+  SkImage,
+  useCanvasRef,
+} from "@shopify/react-native-skia";
+import { api } from "../api/api-client";
+import { useMutation } from "@tanstack/react-query";
+import { notifyMessage } from "../utils/toast-message";
+import { decode as atob } from "base-64";
+import axios from "axios";
 
 interface IPath {
   segments: String[];
@@ -69,6 +80,85 @@ export default function DrawingScreen() {
     }
   };
 
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["create-progress"],
+    mutationFn: async () => {
+      const image = canvasRef.current?.makeImageSnapshot();
+
+      if (!image) throw new Error("No image");
+
+      try {
+        const file = b64toBlob(
+          image.encodeToBase64(ImageFormat.PNG),
+          "image/png"
+        );
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        console.log(formData);
+
+        const res = await api.post("/progress", formData);
+
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    onSuccess: async (data: any) => {
+      // Handle success
+      // console.log(data.data.data);
+    },
+    onError: (error: any) => {
+      // Handle error
+      console.log(error);
+    },
+  });
+
+  const b64toBlob = (b64Data: string, contentType = "", sliceSize = 512) => {
+    const byteCharacters = atob(b64Data);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+
+    const blob = new Blob(byteArrays, { type: contentType });
+    console.log(blob, "1");
+    return blob;
+  };
+
+  const handleSubmit = async () => {
+    // mutate(); // Trigger the mutation
+    const image = canvasRef.current?.makeImageSnapshot();
+
+    if (!image) throw new Error("No image");
+
+    try {
+      const base64 = image.encodeToBase64(ImageFormat.PNG);
+
+      const res = await fetch("data:image/png;base64," + base64);
+
+      console.log(res);
+
+      const blob = await res.blob();
+
+      const fd = new FormData();
+      const file = new File([blob], "filename.png", { type: "image/png" });
+      fd.append("file", file);
+    } catch (er) {
+      console.log(er);
+    }
+  };
+
   return (
     <View className="flex flex-col w-full h-full">
       <Image
@@ -113,6 +203,14 @@ export default function DrawingScreen() {
             className="w-32 rounded-lg flex items-center py-2 bg-primary"
           >
             <Text className="text-gray-200 font-semibold">Undo</Text>
+          </TouchableOpacity>
+        </View>
+        <View className="flex flex-row items-center justify-between mt-4">
+          <TouchableOpacity
+            onPress={handleSubmit}
+            className="w-32 rounded-lg flex items-center py-2 bg-primary"
+          >
+            <Text className="text-gray-200 font-semibold">Submit</Text>
           </TouchableOpacity>
         </View>
       </View>
